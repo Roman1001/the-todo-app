c419e96b4355307ec5ae24930aa735e2955a1ba4
Romein van Buren <romein@vburen.eu>
2020-05-31 15:45:10 +0200

    Initial commit

---
+++ main.js
@@ -0,0 +1,68 @@
+'use strict'
+
+const path = require('path')
+const { app, ipcMain } = require('electron')
+const { BrowserWindow } = require('electron')
+
+const Window = require('./src/class/Window')
+const DataStore = require('./src/class/DataStore')
+
+// create a new todo store name "Todos Main"
+const todosData = new DataStore({ name: 'Todos Main' })
+
+function main () {
+  // todo list window
+  let mainWindow = new Window({
+    file: path.join('views', 'index.html')
+  })
+
+  // add todo window
+  let addTodoWin
+
+  // TODO: put these events into their own file
+
+  // initialize with todos
+  mainWindow.once('show', () => {
+    mainWindow.webContents.send('todos', todosData.todos)
+  })
+
+  // create add todo window
+  ipcMain.on('add-todo-window', () => {
+    // if addTodoWin does not already exist
+    if (!addTodoWin) {
+      // create a new add todo window
+      addTodoWin = new Window({
+        file: path.join('views', 'add.html'),
+        width: 400,
+        height: 400,
+        // close with the main window
+        parent: mainWindow,
+      })
+
+      // cleanup
+      addTodoWin.on('closed', () => {
+        addTodoWin = null
+      })
+    }
+  })
+
+  // add-todo from add todo window
+  ipcMain.on('add-todo', (event, todo, desc) => {
+    const updatedTodos = todosData.addTodo(todo, desc).todos
+
+    mainWindow.send('todos', updatedTodos)
+  })
+
+  // delete-todo from todo list window
+  ipcMain.on('delete-todo', (event, todo) => {
+    const updatedTodos = todosData.deleteTodo(todo).todos
+
+    mainWindow.send('todos', updatedTodos)
+  })
+}
+
+app.on('ready', main)
+
+app.on('window-all-closed', function () {
+  app.quit()
+})
